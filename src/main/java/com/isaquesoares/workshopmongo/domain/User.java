9d620d34e8eb9d597a6bd6d84438a16350627366
Dev-Isaque <isaquesbl123@gmail.com>
2025-02-18 14:43:45 -0300

    Operação de instanciação do banco de dados

--- src/main/java/com/isaquesoares/workshopmongo/domain/User.java
+++ src/main/java/com/isaquesoares/workshopmongo/domain/User.java
@@ -2,9 +2,14 @@ package com.isaquesoares.workshopmongo.domain;
 
 import java.io.Serializable;
 
-public class User implements  Serializable{
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
+    @Id
     private String id;
     private String name;
     private String email;
@@ -65,5 +70,5 @@ public class User implements  Serializable{
         } else if (!id.equals(other.id))
             return false;
         return true;
-    } 
+    }
 }
